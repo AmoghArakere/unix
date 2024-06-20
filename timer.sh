b22f5635e361cead9f2607aaa82c42b06dc43c4b
Amogh <90240269+AmoghArakere@users.noreply.github.com>
2024-06-20 15:27:17 +0530

    Create timer.sh

---
+++ timer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <seconds>"
+  exit 1
+fi
+
+seconds=$1
+
+while [ $seconds -gt 0 ]; do
+  echo -ne "Time remaining: $seconds\033[0K\r"
+  sleep 1
+  seconds=$((seconds - 1))
+done
+
+echo "Time's up!"
+
+/*
+vi timer.sh
+chmod +x timer.sh
+./timer.sh 60
+*/
